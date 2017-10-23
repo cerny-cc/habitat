a13324c80a96c6e0aced75fd25b4c7852ee1a1bb
Nathan Cerny <ncerny@gmail.com>
2017-10-23 08:54:59 -0500

    Working on getting unifi controller running.
    Signed-off-by: Nathan Cerny <ncerny@gmail.com>

--- unifi/plan.sh
+++ unifi/plan.sh
@@ -5,13 +5,16 @@ pkg_maintainer="Nathan Cerny <ncerny@gmail.com>"
 pkg_license=('Apache-2.0')
 pkg_source="http://dl.ubnt.com/${pkg_name}/${pkg_version}/unifi_sysvinit_all.deb"
 pkg_shasum="2a3ea4acd0a9ca01eec5398e2cfd9c582b0b389237f55aa8cd2cccfcd42ee128"
-pkg_deps=(core/glibc core/jdk8/8u131 core/perl)
-pkg_build_deps=(cerny-cc/dpkg cerny-cc/debconf core/diffutils)
-# pkg_lib_dirs=(lib)
+pkg_deps=(core/jre8 cerny-cc/commons-daemon)
+pkg_build_deps=(cerny-cc/dpkg cerny-cc/debconf core/diffutils core/procps-ng core/perl)
+pkg_lib_dirs=(lib)
 # pkg_include_dirs=(include)
-# pkg_bin_dirs=(bin)
+pkg_bin_dirs=(bin)
 # pkg_pconfig_dirs=(lib/pconfig)
-pkg_svc_run="usr/lib/unifi/bin/unifi.init start"
+# pkg_svc_run="usr/lib/unifi/bin/unifi.init start"
+# Export Volumes:
+# /usr/lib/unifi/conf
+# /usr/lib/unifi/dl
 # pkg_exports=(
 #   [host]=srv.address
 #   [port]=srv.port
@@ -19,7 +22,7 @@ pkg_svc_run="usr/lib/unifi/bin/unifi.init start"
 # )
 # pkg_exposes=(port ssl-port)s
 pkg_binds=(
-  [database]="port host"
+  [database]="port"
 )
 # pkg_binds_optional=(
 #   [storage]="port host"
@@ -39,7 +42,18 @@ do_build() {
 }
 
 do_install() {
-  touch "$(pkg_path_for cerny-cc/dpkg)/var/lib/dpkg/status"
+  # Make dpkg think we're always doing a fresh install - beacuse we should be.
+  cat /dev/null > "$(pkg_path_for cerny-cc/dpkg)/var/lib/dpkg/status"
+  ln -fs "$(pkg_path_for cerny-cc/debconf)/usr/share/debconf" /usr/share/
+  ln -fs "$(pkg_path_for core/perl)/bin/perl" /usr/bin/perl
+  export PERL5LIB="$(pkg_path_for cerny-cc/debconf)/usr/share/perl5"
   export DEBIAN_FRONTEND=noninteractive
-  prefix=$pkg_prefix dpkg --force-depends -i $HAB_CACHE_SRC_PATH/$pkg_filename
+  dpkg --force-depends --unpack $HAB_CACHE_SRC_PATH/$pkg_filename
+  mv /usr/lib/unifi/* $pkg_prefix
+  rm /usr/share/debconf
+  rm /usr/bin/perl
+}
+
+do_strip() {
+  return 0
 }
